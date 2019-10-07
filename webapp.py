bd0707fbf5e21c1364563741229b6ae29b9c2732
Blazerwinner <54326437+Blazerwinner@users.noreply.github.com>
2019-10-07 09:45:40 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,7 +2,7 @@ from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/AboutUs")
+@app.route("/")
 def render_main():
     return render_template('home.html')
 
@@ -10,7 +10,7 @@ def render_main():
 def render_page1():
     return render_template('page1.html')
 
-@app.route("/AboutUs")
+@app.route("/")
 def render_page2():
     return render_template('page2.html')
     
