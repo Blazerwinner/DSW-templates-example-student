169bd4aef794249eb0f7b0b084554a8cd2bf2d76
Blazerwinner <54326437+Blazerwinner@users.noreply.github.com>
2019-10-08 09:52:15 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -10,9 +10,13 @@ def render_main():
 def render_page1():
     return render_template('page1.html')
 
-@app.route("/")
+@app.route("/p2")
 def render_page2():
     return render_template('page2.html')
+
+@app.route("/p3")
+def render_page3():
+    return render_template('page3.html')
     
 if __name__=="__main__":
     app.run(debug=False)
